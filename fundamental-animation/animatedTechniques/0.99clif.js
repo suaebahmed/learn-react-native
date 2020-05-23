978e664977873c9579b3a3ffeee7e07ca4e54b9a
suaebahmed <52806080+suaebahmed@users.noreply.github.com>
2020-05-24 00:01:05 +0600

    Update 0.99clif.js

--- fundamental-animation/animatedTechniques/0.99clif.js
+++ fundamental-animation/animatedTechniques/0.99clif.js
@@ -1,3 +1,9 @@
 
 // There are also 0.01 cliff. this can be used
 // to trigger the cliff at the beginning of a animation
+
+componentWillMount (){
+    this.av= new Animated.Value(0);
+    this._panResponder= panResponder.create({})
+
+}
