b3a6b251a8c8ac10e5104fb36506f7767cbd18af
suaebahmed <52806080+suaebahmed@users.noreply.github.com>
2020-05-27 08:33:35 +0600

    Create SwipeCommentModel.js

---
+++ fundamental-animation/advance/SwipeCommentModel.js
@@ -0,0 +1,26 @@
+
+
+
+render(){
+
+  return (
+   <View>
+     <Animated.View style={{}} />
+       <Animated.View>
+         <View> 
+              <ScrollView>
+
+                 <Text></Text>
+                 <View></View>
+                 <Text></Text>
+
+              </ScrollView>
+         </View>
+         <View>
+              <TextInput></TextInput>
+         </View>
+
+       </Animated.View>
+   <View>
+   )
+}
